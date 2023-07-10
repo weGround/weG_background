04278b861e3194dbba15bdac46761a84d0abe91a
YinaePark <dlsoso1@unist.ac.kr>
2023-07-10 13:13:00 +0000

    feat: delete auth folder and add group

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,13 @@ import { BlogFileRepository, BlogMongoRepository } from './blog/blog.repository'
 import { MongooseModule } from '@nestjs/mongoose';
 import { Blog, BlogSchema } from './blog/blog.schema';
 import { SignupService } from './signup/signup.service';
-import { Signup, SignupSchema } from './signup/signup.schema'; // 새로운 Signup 스키마를 가져옴
-import { SignupController } from './signup/signup.controller'; // Signup 컨트롤러를 가져옴
-import { SignupFileRepository, SignupMongoRepository } from './signup/signup.repository'; // Signup 리포지토리를 가져옴
+import { Signup, SignupSchema } from './signup/signup.schema'; 
+import { SignupController } from './signup/signup.controller'; 
+import { SignupFileRepository, SignupMongoRepository } from './signup/signup.repository'; 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { UserinfoController } from './userinfo/userinfo.controller';
-// import { UserinfoModule } from './userinfo/userinfo.module';
-import { AuthController } from './auth/auth.controller';
+import { GroupService } from './group/group.service';
+import { GroupModule } from './group/group.module';
 
 @Module({
   imports: [
@@ -20,11 +19,12 @@ import { AuthController } from './auth/auth.controller';
     MongooseModule.forFeature([{ name: Blog.name, schema: BlogSchema }]),
     MongooseModule.forRoot('mongodb://localhost:27017/signup'),
     MongooseModule.forFeature([{ name: Signup.name, schema: SignupSchema }]),
+    GroupModule,
      // Signup 컬렉션을 등록
   ],
-  controllers: [AppController, BlogController, SignupController, AuthController], // Signup 컨트롤러를 추가
+  controllers: [AppController, BlogController, SignupController], // Signup 컨트롤러를 추가
   providers: [ AppService, 
     BlogService, BlogFileRepository, BlogMongoRepository, 
-    SignupService, SignupFileRepository, SignupMongoRepository], // Signup 리포지토리를 추가
+    SignupService, SignupFileRepository, SignupMongoRepository, GroupService], // Signup 리포지토리를 추가
 })
 export class AppModule {}
--- src/auth/auth.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { SignupModule } from 'src/signup/signup.module'
-
-@Module({
-  imports: [SignupModule],
-  providers: [AuthService]
-})
-export class AuthModule {}
